4bc9c757e263d17f6247c40da8014fccbc4630db
klu0926 <lukuoyu@gmail.com>
2025-02-28 10:41:48 -0500

    feat: render projects from data

--- script.js
+++ script.js
@@ -1,18 +1,78 @@
 import { getPosts } from "./src/getPortfolioData.js"
 
-async function getPostsAsync() {
-  try {
-    const { err, posts } = await getPosts()
-    if (err) throw new Error(err)
-
-    console.log('Posts: ', posts)
-
-  } catch (err) {
-    console.error(err)
+class Data {
+  constructor() {
+  }
+  async getPostsAsync() {
+    try {
+      const { err, posts } = await getPosts()
+      if (err) throw new Error(err)
+      return posts
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
+
+class View {
+  constructor() {
+    this.projectContainer = document.querySelector('.projects-container')
+  }
+  renderProjects(projectsArray, filterGroup = '') {
+    let filteredProjects = []
+
+    // clear container
+    this.projectContainer.innerHTML = ''
+
+    // filter
+    if (filterGroup !== '') {
+      filteredProjects = projectsArray.filter(project => project.group === filterGroup)
+    } else {
+      filteredProjects = projectsArray
+    }
+
+    // Render project cards
+    filteredProjects.forEach(project => {
+      // create porject card
+      const projectCard = `
+       <div class="project-card">
+            <img
+              class="project-image"
+              src="${project.cover}"
+              alt="project image"
+            />
+            <p class="project-name">${project.title}</p>
+            <p class="project-info">${project.description}</p>
+          </div>
+      `
+      // add to container
+      this.projectContainer.innerHTML += projectCard
+    })
+  }
+}
+
+
+class Controller {
+  constructor(data, view) {
+    this.data = data
+    this.view = view
+  }
+  async init() {
+    // render posts
+    const posts = await this.data.getPostsAsync()
+    this.view.renderProjects(posts, 'web')
+    console.log(posts)
+  }
+}
+
+
+// Run 
+const data = new Data()
+const view = new View()
+const controller = new Controller(data, view)
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  getPostsAsync()
+  controller.init()
 })
